11aa4d663902639465021b4bd1838225c8720dd4
beans <ujt2h8@gmail.com>
2019-07-12 07:18:12 +0800

    feat : implement practice05, practice06, practice07

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,28 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private int klass = -1;
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+
+
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+
+    public String introduce() {
+        if(klass == -1)
+            return super.introduce() + " I am a Teacher. " + "I teach No Class." ;
+        else
+            return super.introduce() + " I am a Teacher. " + "I teach "+"Class "+klass+"." ;
+
+    }
 }
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,39 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+
+    }
+
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        if(klass == null)
+            return super.introduce() + " I am a Teacher. " + "I teach No Class.";
+        else
+            return super.introduce() + " I am a Teacher. " + "I teach "+klass.getDisplayName()+"." ;
+
+    }
+
+
+    public String introduceWith(Student student) {
+
+
+        if (student.getKlass() == klass) {
+            return super.introduce() + " I am a Teacher. " + "I teach " + student.getName() + ".";
+        }else {
+            return super.introduce() + " I am a Teacher. " + "I don't teach " + student.getName() + ".";
+        }
+    }
 }
